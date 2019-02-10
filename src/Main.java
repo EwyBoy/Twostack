4e2f8a5408e45d9aab738cab3fb67c38d6f8b663
EwyBoy <ewyboy@gmail.com>
2019-02-10 23:13:04 +0100

    done

--- src/Main.java
+++ src/Main.java
@@ -10,12 +10,19 @@ public class Main {
     }
 
     private static boolean isOperator(char c) {
+
         String operators = "-+*/";
+
         for (int i = 0; i < operators.length(); i++) {
+
             if (operators.contains(Character.toString(c))) {
+
                 return true;
+
             }
+
         }
+
         return false;
     }
 
@@ -40,10 +47,10 @@ public class Main {
         String operand_one = twostack.pop(operand);
         String operand_two = twostack.pop(operand);
 
-        char operator_one = twostack.pop(operator).charAt(0);
+        String operator_one = twostack.pop(operator);
 
-        String tmp = operator_one + operand_two + operand_one;
-        twostack.push(operand, tmp);
+        String prefix = operator_one + operand_two + operand_one;
+        twostack.push(operand, prefix);
     }
 
 
@@ -53,19 +60,22 @@ public class Main {
 
         for (int i = 0; i < trimmedInfix.length(); i++) {
 
-            if (!isOperator(trimmedInfix.charAt(i))) {
+            char c = trimmedInfix.charAt(i);
+
+            if (!isOperator(c)) {
 
                 twostack.push(operand,trimmedInfix.charAt(i) + "");
 
             } else {
 
-                while (!twostack.isEmpty(operator) && getOperatorPrecedent(trimmedInfix.charAt(i)) <= getOperatorPrecedent(peekChar(operator))) {
+                while (!twostack.isEmpty(operator) && getOperatorPrecedent(c) <= getOperatorPrecedent(peekChar(operator))) {
 
                     shuffle();
 
                 }
 
-                twostack.push(operator, String.valueOf(trimmedInfix.charAt(i)));
+                twostack.push(operator, String.valueOf(c));
+
             }
         }
 
@@ -79,80 +89,9 @@ public class Main {
         return twostack.peek(operand);
     }
 
-
-    private static void debug(Boolean side) {
-        String sideName = side ? "Right" : "Lefty";
-        System.out.println("Stack: " + sideName + "\t-\t" + twostack.peek(side) + "\t-\t Stack Size: " + twostack.size(side));
-    }
-
     public static void main(String[] args) {
         String infix = "A + B * C";
         System.out.println("Final result: " + infixToPrefix(infix));
-    }
-
-    //https://stackoverflow.com/questions/31562506/implement-two-stacks-using-one-array
-    private void test() {
-        System.out.println("-------PUSH RIGHT-------");
-
-        twostack.push(true, "apple");
-        debug(true);
-        twostack.push(true, "mango");
-        debug(true);
-        twostack.push(true, "melon");
-        debug(true);
-
-        System.out.println("-------PUSH LEFT-------");
-
-        twostack.push(false, "carrot");
-        debug(false);
-        twostack.push(false, "potato");
-        debug(false);
-        twostack.push(false, "salad");
-        debug(false);
-
-        System.out.println("-------POP RIGHT-------");
-
-        debug(true);
-        twostack.pop(true);
-        debug(true);
-        twostack.pop(true);
-        debug(true);
-        twostack.pop(true);
-
-        System.out.println("-------POP LEFT-------");
-
-        debug(false);
-        twostack.pop(false);
-        debug(false);
-        twostack.pop(false);
-        debug(false);
-        twostack.pop(false);
-
-        System.out.println("-------CLEAR-------");
-        twostack.push(true, "1");
-        twostack.push(true, "2");
-        twostack.push(true, "3");
-        twostack.push(true, "4");
-        twostack.push(true, "5");
-        debug(true);
-
-        twostack.push(false, "1");
-        twostack.push(false, "2");
-        twostack.push(false, "3");
-        twostack.push(false, "4");
-        twostack.push(false, "5");
-        debug(false);
-
-        twostack.clear();
-
-        debug(true);
-        debug(false);
-
-        twostack.push(true, "cat");
-        twostack.push(false, "dog");
-
-        debug(true);
-        debug(false);
     }
 
 }
--- src/TwostackArray.java
+++ src/TwostackArray.java
@@ -1,9 +1,6 @@
 import exceptions.TwostackEmptyException;
 import exceptions.TwostackFullException;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class TwostackArray<E> implements Twostack<E> {
 
     private E[] twostack;
@@ -19,7 +16,7 @@ public class TwostackArray<E> implements Twostack<E> {
 
     public TwostackArray(int maxStackSize) {
         twostack = (E[]) new Object[maxStackSize + 1];
-        leftArraySize = (0);
+        leftArraySize = 0;
         rightArraySize = twostack.length;
     }
 
@@ -33,26 +30,32 @@ public class TwostackArray<E> implements Twostack<E> {
 
     @Override
     public Integer size(Boolean right) {
-        if (right) {
-            return twostack.length - rightArraySize;
-        } else {
-            return leftArraySize;
-        }
+        return right ? twostack.length - rightArraySize : leftArraySize;
     }
 
     @Override
     public void push(Boolean right, E element) throws TwostackFullException {
         if (right) {
+
             if (rightArraySize - leftArraySize > 1) {
+
                 twostack[--rightArraySize] = element;
+
             } else {
+
                 throw new TwostackFullException();
+
             }
         } else {
+
             if (rightArraySize - leftArraySize > 1) {
+
                 twostack[++leftArraySize] = element;
+
             } else {
+
                 throw new TwostackFullException();
+
             }
         }
     }
@@ -62,22 +65,35 @@ public class TwostackArray<E> implements Twostack<E> {
         E topElement;
 
         if (right) {
+
             if (rightArraySize < twostack.length) {
+
                 topElement = peek(true);
                 twostack[rightArraySize] = null;
                 rightArraySize++;
+
                 return topElement;
+
             } else {
+
                 throw new TwostackEmptyException();
+
             }
+
         } else {
-            if (leftArraySize > (0)) {
+
+            if (leftArraySize > 0) {
+
                 topElement = peek(false);
                 twostack[leftArraySize] = null;
                 leftArraySize--;
+
                 return topElement;
+
             } else {
+
                 throw new TwostackEmptyException();
+
             }
         }
     }
@@ -86,43 +102,67 @@ public class TwostackArray<E> implements Twostack<E> {
     public E peek(Boolean right) throws TwostackEmptyException {
 
         if (rightArraySize == twostack.length) {
+
             rightArraySize--;
+
         }
         if (leftArraySize == 0) {
+
             leftArraySize++;
+
         }
 
         if (right) {
+
             if (rightArraySize < twostack.length) {
+
                 return twostack[rightArraySize];
+
             } else {
+
                 throw new TwostackFullException();
+
             }
+
         } else {
-            if (leftArraySize > (0)) {
+
+            if (leftArraySize > 0) {
+
                 return twostack[leftArraySize];
+
             } else {
+
                 throw new TwostackEmptyException();
+
             }
         }
     }
 
     @Override
     public Boolean contains(E element) {
+
         boolean isFound = false;
-        for (int i = 0; i < twostack.length; i++) {
-            if (element.equals(twostack[i])) {
+
+        for (E e : twostack) {
+
+            if (element.equals(e)) {
+
                 isFound = true;
                 break;
+
             }
         }
+
         return isFound;
     }
 
     @Override
     public void clear() {
+
         for (int i = 0; i < twostack.length; i++) {
+
             twostack[i] = null;
+
         }
 
         rightArraySize = twostack.length;
